b5971c138960a7c0e5f9e42502fb65d21d044940
Kevin Mateo <gomezdiezkevinmateo@gmail.com>
2024-08-22 11:04:00 -0500

    add in todo funtions init, mark_completed and add_tag

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,26 @@
 # TODO: Add code here
+class Todo:
+
+    def __init__(self,code_id: int, title: str,descripction: str):
+        self.code_id: int= code_id
+        self.title: str= title
+        self.description: str= descripction
+        self.completed: bool= False
+        self.tags: list[str]= []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+        def __str__
+
+
+
+
+
+
+
+
+
