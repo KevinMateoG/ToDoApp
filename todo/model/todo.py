a46dc6395510d7ae9727a352b9516d0d0959201c
Kevin Mateo <gomezdiezkevinmateo@gmail.com>
2024-08-22 11:30:49 -0500

    implement in Todo __str__ and Create class TodoBook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -14,9 +14,12 @@ class Todo:
     def add_tag(self, tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
-        def __str__
-
-
+    def __str__(self)->str:
+        return f"el codigo es:{self.code_id} - y el titulo es {self.title}"
+    class TodoBook:
+        def __init__(self):
+            self.todos: dict[int, Todo] = {}
+        #def add_todo(self, title: str,description: str):
 
 
 
